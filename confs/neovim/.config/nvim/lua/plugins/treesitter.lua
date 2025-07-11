c125c1d2109a06510e489e043d4ffe21fd7609ef
Carlos Rosquillas <crosquillas@gmail.com>
2025-07-11 11:53:46 -0700

    feat: Add endwise for Treesitter

--- confs/neovim/.config/nvim/lua/plugins/treesitter.lua
+++ confs/neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,9 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "RRethy/nvim-treesitter-endwise",
+    },
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "cpp",
