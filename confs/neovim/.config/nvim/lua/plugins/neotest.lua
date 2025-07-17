b46af8c25a80eb66b24c9d571902b7a54385584b
Carlos Rosquillas <crosquillas@gmail.com>
2025-07-17 12:36:04 -0700

    feat: Add Ruby support to Neotest

--- confs/neovim/.config/nvim/lua/plugins/neotest.lua
+++ confs/neovim/.config/nvim/lua/plugins/neotest.lua
@@ -1,14 +1,22 @@
 return {
+  -- TODO: See if these are covered by extras, add as dependency
   { "nvim-neotest/neotest-jest" },
   { "jfpedroza/neotest-elixir" },
   { "nvim-neotest/neotest-python" },
+  -- TODO: Remove since it is handled by extras, redefine as dependency of neotest
+  { "olimorris/neotest-rspec" },
   {
     "nvim-neotest/neotest",
     config = function(_, opts)
       vim.list_extend(opts.adapters, {
+        -- Javascript
         require("neotest-jest"),
+        -- Elixir
         require("neotest-elixir"),
+        -- Python
         require("neotest-python"),
+        -- Ruby
+        require("neotest-rspec"),
       })
       require("neotest").setup(opts)
     end,
