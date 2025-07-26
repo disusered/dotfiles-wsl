addbd206fde9bd3cac50f6cc8728367dd710cf9f
Carlos Rosquillas <crosquillas@gmail.com>
2025-07-25 21:39:09 -0700

    feat: Add keymaps for DAP virtual text

---
+++ confs/neovim/.config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,15 @@
+return {
+  "mfussenegger/nvim-dap",
+  keys = {
+    {
+      "<leader>dv",
+      "<cmd>DapVirtualTextToggle<cr>",
+      desc = "Toggle virtual text",
+    },
+    {
+      "<leader>dV",
+      "<cmd>DapVirtualTextForceRefresh<cr>",
+      desc = "Refresh virtual text",
+    },
+  },
+}
